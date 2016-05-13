64cd941e5ac847ba9e893619293e5085a8793b18
mashybuttons <katherine.sj@gmail.com>
2016-05-12 20:40:36 -0700

    promises are donee

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -7,14 +7,22 @@
  *
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
-
-var fs = require('fs');
 var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require("fs"));
+var profile = require('./promisification.js');
+var firstline = require('./promiseConstructor.js');
 
-
-
+// pluckFirstLineFromFileAsync
+// getGitHubProfileAsync
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  return firstline.pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(username) {
+      return profile.getGitHubProfileAsync(username);
+    }).then(function(data) {
+      return fs.appendFile(writeFilePath, JSON.stringify(data));
+    }).catch(function(error) { throw new Error(error); });
+
 };
 
 // Export these functions so we can test them
